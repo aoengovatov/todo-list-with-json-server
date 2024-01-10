1fe7b99a10c7ef02a8429c8d244e20bc0215c695
AlexEngo <a.o.engovatov@yandex.ru>
2024-01-10 21:17:08 +0400

    init redux & add store, reducer

---
+++ src/store/todos-reducer.js
@@ -0,0 +1,12 @@
+export const todosReducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            return null;
+        case "DELETE_TODO":
+            return null;
+        case "UPDATE_TODO":
+            return null;
+        default:
+            return state;
+    }
+};
